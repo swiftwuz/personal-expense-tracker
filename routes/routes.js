2d648627c6837ee20d92a9c314cde26ac87278f6
Nana Kojo Ewusie <enanakojo@icloud.com>
2022-12-07 03:06:40 +0000

    description for routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,28 @@ const userIncomeRoutes = require("./user-income");
 
 const appRouter = (app, fs) => {
   app.get("/", (req, res) => {
-    res.send("welcome to the dev server");
+    let message = `
+    welcome to the personal expense tracker server
+
+    base_url: https://personal-expense-tracker.up.railway.app
+    user routes (endpoints):
+      signup: POST /auth/signup
+      login: POST /auth/login
+      profile: GET /user/:id
+
+    expenditure routes (endpoints):
+      get expenditures: GET /user/expenditures
+      add expenditure: POST /user/expenditure
+      get detail expenditure: GET /user/expenditure/:id
+      delete expenditure: DELETE /user/expenditure/:id
+
+    income routes (endpoints):
+      get income: GET /user/income
+      add expenditure: POST /user/income
+      get detail expenditure: GET /user/income/:id
+      delete expenditure: DELETE /user/income/:id
+    `;
+    res.send(message);
   });
 
   userRoutes(app, fs);
