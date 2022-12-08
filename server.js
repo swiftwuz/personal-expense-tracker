bb47bc2792f193e425d247476ada7be4db09ed11
Nana Kojo Ewusie <enanakojo@icloud.com>
2022-12-08 15:55:40 +0000

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
 
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
